81185b1d0dae37f164774c3a85e2aaa66868e157
wayneliu <1005881270@qq.com>
2019-04-22 00:32:55 +0800

    更新函数

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 # @File    : main.py
 # @Project : xiaoyou
 
-from flask import Flask, request,jsonify
+from flask import Flask, request, jsonify
 from common.db import db_session
 import time
 import sys
@@ -30,34 +30,110 @@ def create_roles():
 
 def get_latest_act_id():
     '''获取最近一次的活动id'''
-    sql = "select a_id from activities order by a_id desc"
+    sql = "select a_id from activities where CHAR_LENGTH(a_id)=(SELECT max(CHAR_LENGTH(a_id)) from activities) ORDER BY a_id DESC"
     res = db_session.execute(sql).fetchone()
     return res.a_id
+
+def get_latest_content_id():
+    '''获取最近一次的活动内容id'''
+    sql = "select c_id from act_content where CHAR_LENGTH(c_id)=(SELECT max(CHAR_LENGTH(c_id)) from act_content) ORDER BY c_id DESC"
+    res = db_session.execute(sql).fetchone()
+    return res.c_id
 
 @app.route('/api/activities', methods=['GET', 'POST'])
 def create_act():
     '''创建活动'''
     if request.method == 'POST':
         latest_id = get_latest_act_id()
-        a_id = 'a' + str(int(latest_id[1:]) + 1)
-        act_title = request.get_json().get('act_title')
-        act_desc = request.get_json().get('act_desc')
-        date_start = request.get_json().get('date_start')
-        date_end = request.get_json().get('date_end')
-        act_contact = request.get_json().get('act_contact')  # 活动联系人
-        is_all = request.get_json().get('is_all')
-        print a_id
-        sql = "insert into activities (a_id,act_title,act_desc,date_start,date_end,act_contact,is_all)" \
-              "values (:a_id,:act_title,:act_desc,:date_start,:date_end,:act_contact,:is_all)"
-        db_session.execute(sql, {'a_id':a_id,'act_title':act_title,'act_desc':act_desc,'date_start':date_start,
-                                 'date_end':date_end,'act_contact':act_contact,'is_all':is_all})
+        a_id = 'a' + str(int(latest_id[1:]) + 1) #自动设置最新的活动id
+        print "request.get_json().get('actForm')", request.get_json().get('actForm')
+        print "request.get_json().get('contForm')", request.get_json().get('contForm')
+        # 活动内容列表
+        cont_form = request.get_json().get('contForm')
+        # 创建活动
+        act_form = request.get_json().get('actForm')
+        act_title = act_form.get('act_title')
+        act_desc = act_form.get('act_desc')
+        date_start = act_form.get('date_start').split('T')[0]
+        date_end = act_form.get('date_end').split('T')[0]
+        act_contact = act_form.get('act_contact')  # 活动联系人
+        is_all = act_form.get('is_all')
+        print 'type(a_id)', type(a_id), 'a_id',a_id
+        print 'type(act_title)', type(act_title), 'act_title',act_title
+        print 'type(act_desc)', type(act_desc), 'act_desc',act_desc
+        print 'type(date_start)', type(date_start), 'date_start',date_start
+        print 'type(date_end)', type(date_end), 'date_end',date_end
+        print 'type(act_contact)', type(act_contact), 'act_contact',act_contact
+        print 'type(is_all)', type(is_all), 'is_all',is_all
+        try:
+            # 新建活动
+            sql = "insert into activities (a_id,act_title,act_desc,date_start,date_end,act_contact,is_all)" \
+                  "values (:a_id,:act_title,:act_desc,:date_start,:date_end,:act_contact,:is_all)"
+            db_session.execute(sql, {'a_id':a_id,'act_title':act_title,'act_desc':act_desc,'date_start':date_start,
+                                     'date_end':date_end,'act_contact':act_contact,'is_all':is_all})
+            db_session.commit()
+            # 新建活动内容
+            create_act_cont(a_id, cont_form)
+            return jsonify({'code': '1', 'msg': '创建活动成功！'})
+        except Exception as e:
+            print 'create_act=>ERROR,str(e): ', str(e)
+            return jsonify({'code': '0', 'msg': '创建活动失败，请联系管理员！'})
+
+
+def create_a2c_relationship(a_id, c_ids):
+    """创建活动与活动内容的关联"""
+    if a_id and c_ids:
+        for c_id in c_ids:
+            sql = """insert into act_has_cont (a_id, c_id) values (:a_id, :c_id)"""
+            try:
+                db_session.execute(sql, {'a_id': a_id, 'c_id': c_id})
+            except Exception as e:
+                print 'create_a2c_relationship=>ERROR,str(e): ', str(e)
         db_session.commit()
-        return jsonify({'code':'1', 'msg':'创建活动成功！'})
 
 
-def create_act_cont():
+
+def create_act_cont(a_id, cont_form):
     '''创建活动内容'''
-    pass
+    # 创建活动
+    print 'create_act_cont=>a_id', a_id
+    latest_cid = get_latest_content_id()  # 已存在的最大c_id  c4
+    c_ids = []  # 此a_id的所有活动内容的id
+    activates = cont_form.get('activates')
+    step = 1
+    for act_cont in activates:
+        c_id = 'c' + str(int(latest_cid[1:])+step)
+        c_ids.append(c_id)
+        charger = act_cont.get('charger')
+        company = act_cont.get('company')
+        start_time = act_cont.get('start_time')
+        end_time = act_cont.get('end_time')
+        bus_station = act_cont.get('bus_station')
+        gps = act_cont.get('gps')
+        address = act_cont.get('address')
+        print 'c_id', c_id
+        print 'type(charger)', type(charger), 'charger', charger
+        print 'type(company)', type(company), 'company', company
+        print 'type(start_time)', type(start_time), 'start_time', start_time
+        print 'type(bus_station)', type(bus_station), 'bus_station', bus_station
+        print 'type(end_time)', type(end_time), 'end_time', end_time
+        print 'type(gps)', type(gps), 'gps', gps
+        print 'type(address)', type(address), 'address', address
+        # 插入活动
+        sql = """insert into act_content (c_id,charger,company,time_start,time_end,addr,gps,bus_station) 
+        values (:c_id,:charger,:company,:time_start,:time_end,:addr,:gps,:bus_station)"""
+        try:
+            db_session.execute(sql, {
+             'c_id': c_id, 'charger': charger, 'company': company, 'time_start': start_time,
+                'time_end': end_time, 'addr': address, 'gps': gps, 'bus_station': bus_station
+            })
+        except Exception as e:
+            print 'create_act_cont=>ERROR,str(e): ', str(e)
+        step += 1
+    # 全部插入后才提交
+    db_session.commit()
+    # 创建活动与活动内容的关联
+    create_a2c_relationship(a_id, c_ids)
 
 
 def check(phone, a_id):
@@ -161,8 +237,8 @@ def insert_data():
         else:
             has_car = 'no'
         for c_id in c_ids:
-            text = "insert into join_act_cont (phone,c_id,drive_people,drive_car,insert_time) values ('%s','%s','%s','%s','%s')" % (phone,c_id,
-                                                                                                                   passenger,has_car,insert_time)
+            text = "insert into join_act_cont (phone,c_id,drive_people,drive_car,insert_time) " \
+                   "values ('%s','%s','%s','%s','%s')" % (phone,c_id,passenger,has_car,insert_time)
             db_session.execute(text)
             db_session.commit()
         return jsonify({'code':'1', 'msg':'报名成功！'})
